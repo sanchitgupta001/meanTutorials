b25ac5815402a5ac536b700e00eb6559df58b9df
Sanchit Gupta <sanchit.gupta@sprinklr.com>
2017-11-19 18:10:03 +0530

    Added code for hashing password

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs'); // For hashing of the passwords
 
 var UserSchema = new Schema({
 	name: String,
@@ -8,5 +9,24 @@ var UserSchema = new Schema({
 	password: {type: String, required: true, select: false} // select: false - To avoid password fetching while querying details
 });
 
+// For Hashing password field
+UserSchema.pre('save', function(next){ // consult http://mongoosejs.com/docs/middleware.html
+	var user = this;
 
+	if(!user.isModified('password'))
+		return next();
+
+	bcrypt.hash(user.password, null, null, function(err, hash){ // consult https://github.com/shaneGirish/bcrypt-nodejs
+
+		if(err){
+			return next(err);
+		}
+
+		user.password = hash;
+		next();
+
+	});
+
+
+});
 module.exports = mongoose.model('User', UserSchema);
